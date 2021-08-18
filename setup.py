f6b4b7b09e5eacefde2307fe72ab747a3ac772d6
Robin Mueller <robin.mueller.m@gmail.com>
2021-08-18 12:02:29 +0200

    fixed setup files

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
 We do the package handling in the static setup.cfg but include an empty setup.py
 to allow editable installs https://packaging.python.org/tutorials/packaging-projects/
